9765faf557a409bdff4d6680e0725c7d4e8c9bee
victorpere <paninaro095@gmail.com>
2020-04-21 21:50:42 -0400

    Save gameId in connectionsTable on create

--- connect_handler.py
+++ connect_handler.py
@@ -8,6 +8,6 @@ def handle(event, context):
     connectionId = event['requestContext']['connectionId']
 
     # Insert the connectionId of the connected device to the database
-    dynamodb.put_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Item={'connectionId': {'S': connectionId}, 'connectionName': {'S': 'new connection'}})
+    dynamodb.put_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Item={'connectionId': {'S': connectionId}})
 
     return {}
--- on_creategame_handler.py
+++ on_creategame_handler.py
@@ -8,11 +8,25 @@ dynamodb = boto3.client('dynamodb')
 
 def handle(event, context):
     connectionId = event['requestContext']['connectionId']
+    playerName = json.loads(event['body'])['creator']
     gameId = str(uuid.uuid4())
 
     # Create a new record for the game
     dynamodb.put_item(TableName=os.environ['GAMES_TABLE_NAME'], Item={'gameId': {'S': gameId}})
     
+    # Update the connection record with the gameId and playerName
+    connectionsTableName = os.environ['SOCKET_CONNECTIONS_TABLE_NAME']
+    dynamodb.update_item(
+        TableName=connectionsTableName,
+        Key={ 'connectionId': {'S': connectionId}},
+        UpdateExpression="set gameId = :s, playerName = :p",
+        ExpressionAttributeValues={
+            ':s': { 'S': gameId },
+            ':p': { 'S': playerName }
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
     # Send the gameId to the creator
     apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', 
     endpoint_url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
