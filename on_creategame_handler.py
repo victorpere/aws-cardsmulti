d7333baaf2f70737527254bd64cdd735e0b6fad1
victorpere <paninaro095@gmail.com>
2020-04-25 17:16:34 -0400

    Save and return gameCode

--- on_creategame_handler.py
+++ on_creategame_handler.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import os
 import uuid
+import random
 
 dynamodb = boto3.client('dynamodb')
 
@@ -10,9 +11,10 @@ def handle(event, context):
     connectionId = event['requestContext']['connectionId']
     playerName = json.loads(event['body'])['creator']
     gameId = str(uuid.uuid4())
+    gameCode = str(random.randrange(1000,9999))
 
     # Create a new record for the game
-    dynamodb.put_item(TableName=os.environ['GAMES_TABLE_NAME'], Item={'gameId': {'S': gameId}})
+    dynamodb.put_item(TableName=os.environ['GAMES_TABLE_NAME'], Item={'gameId': {'S': gameId}, 'gameCode': {'S': gameCode}, 'creator': {'S': playerName}})
     
     # Update the connection record with the gameId and playerName
     connectionsTableName = os.environ['SOCKET_CONNECTIONS_TABLE_NAME']
@@ -32,7 +34,7 @@ def handle(event, context):
     endpoint_url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
 
     apigatewaymanagementapi.post_to_connection(
-            Data=json.dumps({'gameId': gameId}),
+            Data=json.dumps({'gameId': gameId, 'gameCode': gameCode}),
             ConnectionId=connectionId
         )
 
