ac80e9c9110c666e7f7c69657f83770048b7f82a
victorpere <paninaro095@gmail.com>
2020-04-26 16:07:46 -0400

    Join game function

--- on_joingame_handler.py
+++ on_joingame_handler.py
@@ -3,25 +3,40 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
 
 def handle(event, context):
+    # Setup
+    connectionsTable = dynamodb.Table(os.environ['SOCKET_CONNECTIONS_TABLE_NAME'])
+    gamesTable = dynamodb.Table(os.environ['GAMES_TABLE_NAME'])
+    apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', 
+    endpoint_url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
+
+    # Request content
     connectionId = event['requestContext']['connectionId']
     requestBody = json.loads(event['body'])
     playerName = requestBody['playerName']
     gameCode = requestBody['gameCode']
 
-    # Find matching game
-    gamesTable = dynamodb.Table(os.environ['GAMES_TABLE_NAME'])
+    # Retrieve the game
     games = gamesTable.query(
+        IndexName='gameCodeIndex',
         KeyConditionExpression=Key('gameCode').eq(gameCode)
     )
 
-    
-
-    # Update gameId
+    # No matching games
+    if games['Count'] == 0:
+        apigatewaymanagementapi.post_to_connection(
+                Data=json.dumps({'status': 'Game not found', 'gameCode': gameCode}),
+                ConnectionId=connectionId
+            )
+        return {}
 
     # Send connectionIds to all players in the game
+    apigatewaymanagementapi.post_to_connection(
+            Data=json.dumps({'status': 'Game found', 'gameCode': gameCode}),
+            ConnectionId=connectionId
+        )
 
     return {}
