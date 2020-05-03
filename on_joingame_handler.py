588a406d28d03daae93473bbd8fe0211146833a3
victorpere <paninaro095@gmail.com>
2020-05-03 15:46:54 -0400

    Update connection record with gameId on join

--- on_joingame_handler.py
+++ on_joingame_handler.py
@@ -4,10 +4,11 @@ import os
 from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
-
+dbclient = boto3.client('dynamodb')
 
 def handle(event, context):
     # Setup
+    connectionsTableName = os.environ['SOCKET_CONNECTIONS_TABLE_NAME']
     connectionsTable = dynamodb.Table(os.environ['SOCKET_CONNECTIONS_TABLE_NAME'])
     gamesTable = dynamodb.Table(os.environ['GAMES_TABLE_NAME'])
     apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', 
@@ -37,6 +38,19 @@ def handle(event, context):
     game = games['Items'][0]
     gameId = game['gameId']
     creator = game['creator']
+    players = str(game['players'])
+
+    # Update the connection record with the gameId and playerName
+    dbclient.update_item(
+        TableName=connectionsTableName,
+        Key={ 'connectionId': {'S': connectionId}},
+        UpdateExpression="set gameId = :s, playerName = :p",
+        ExpressionAttributeValues={
+            ':s': { 'S': gameId },
+            ':p': { 'S': playerName }
+        },
+        ReturnValues="UPDATED_NEW"
+    )
 
     # Send connectionIds to all players in the game
     apigatewaymanagementapi.post_to_connection(
@@ -44,7 +58,9 @@ def handle(event, context):
                 'status': 'Game found', 
                 'gameCode': gameCode,
                 'gameId': gameId,
-                'creator': creator
+                'creator': creator,
+                'players': players,
+                'playerName': playerName
             }),
             ConnectionId=connectionId
         )
