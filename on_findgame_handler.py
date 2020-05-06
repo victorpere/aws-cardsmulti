03e975779089d9a361ec75e93646c7820d8c1eb7
victorpere <paninaro095@gmail.com>
2020-05-06 18:55:51 -0400

    Method to find game by code

---
+++ on_findgame_handler.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+import os
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb')
+dbclient = boto3.client('dynamodb')
+
+def handle(event, context):
+    # Setup
+    gamesTable = dynamodb.Table(os.environ['GAMES_TABLE_NAME'])
+    apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', 
+    endpoint_url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
+
+    # Request content
+    connectionId = event['requestContext']['connectionId']
+    requestBody = json.loads(event['body'])
+    gameCode = requestBody['gameCode']
+
+    # Retrieve the games
+    games = gamesTable.query(
+        IndexName='gameCodeIndex',
+        KeyConditionExpression=Key('gameCode').eq(gameCode)
+    )
+
+    # Create list of gameIds
+    gameIds = []
+    for game in games['Items']:
+        gameIds.append({ 'gameId': game['gameId'], 'creator': game['creator'] })
+
+    # Send back list of gameIds:
+    apigatewaymanagementapi.post_to_connection(
+            Data=json.dumps({
+                'status': 'GamesList', 
+                'gameIds': gameIds
+            }),
+            ConnectionId=connectionId
+        )
+
+    return {}
