3e1b0f020ce01b96eba7259345b32334ca6119da
victorpere <paninaro095@gmail.com>
2020-05-17 13:05:19 -0400

    Send message to specified recepients only

--- on_message_handler.py
+++ on_message_handler.py
@@ -6,23 +6,28 @@ dynamodb = boto3.client('dynamodb')
 
 
 def handle(event, context):
-    connectionId = event['requestContext']['connectionId']
-    message = json.loads(event['body'])['message']
-    paginator = dynamodb.get_paginator('scan')
-    connectionIds = []
-
+    # Setup
     apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', 
     endpoint_url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
 
-    # Retrieve all connectionIds from the database
-    for page in paginator.paginate(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME']):
-        connectionIds.extend(page['Items'])
+    # Request content
+    senderConnectionId = event['requestContext']['connectionId']
+    body = json.loads(event['body'])
+    message = body['message']
+    recepients = body['recepients']
+    connectionIds = recepients.split(",")
 
     # Emit the recieved message to all the connected devices
     for connectionId in connectionIds:
-        apigatewaymanagementapi.post_to_connection(
-            Data=message,
-            ConnectionId=connectionId['connectionId']['S']
-        )
+        if connectionId and connectionId != senderConnectionId:
+            apigatewaymanagementapi.post_to_connection(
+                Data=json.dumps({ 
+                    "status": "Message",
+                    "sender": senderConnectionId,
+                    "message": message,
+                    "recepients": recepients
+                }),
+                ConnectionId=connectionId
+            )
 
     return {}
